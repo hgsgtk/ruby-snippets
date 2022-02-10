e4bbe9fa58306ce989290b77de9d06204ef827f8
hgsgtk <kazuki@autify.com>
2022-02-10 10:22:20 +0900

    ActiveSupport's Array wrap methods

---
+++ array-1.rb
@@ -0,0 +1,6 @@
+# https://www.digitalocean.com/community/tutorials/how-to-write-your-first-ruby-program
+
+s = "s"
+# https://gorails.com/episodes/ruby-array-wrap
+# > ActiveSupport's Array wrap methods
+ss = Array.wrap(s)
