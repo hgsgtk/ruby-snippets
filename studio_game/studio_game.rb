9482931e25d544ea73df0352991f58fdb6dec4fd
hgsgtk <kazuki@autify.com>
2025-08-14 16:35:33 +0900

    Add Player class to manage player attributes and actions

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,6 +1,35 @@
-def say_hello(name:, health: 100)
-  "I'm #{name.capitalize} with a health of #{health}"
+class Player
+    def initialize(name, health = 100)
+        @name = name.capitalize
+        @health = health
+    end 
+    
+    def to_s = "I'm #{@name} with a health of #{@health}"
+
+    def drain
+        @health -= 10
+    end
+
+    def boost
+      @health += 15
+    end
 end
 
-puts say_hello(name: "finn", health: 60)
-puts say_hello(name: "jake")
+player_1 = Player.new('finn', 60)
+puts player_1.inspect
+puts player_1
+
+lucy = Player.new('lucy', 90)
+jase = Player.new('jase', 100)
+alex = Player.new('alex', 125)
+puts lucy
+puts jase
+puts alex
+
+alex.drain
+puts alex
+
+alex.boost
+puts alex
+
+puts player_1
