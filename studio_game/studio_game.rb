ecf09f34d2421855b9799c2f7259107b43a8116f
hgsgtk <kazuki@autify.com>
2025-08-14 16:09:15 +0900

    enhance formatting in game output

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,7 +1,12 @@
 puts "Let's play a game\n\n\t3\n\t2\n\t1\n\n"
 
-first_player = "Finn"
-first_player_health = 60
-puts "#{first_player}'s health is #{first_player_health / 9}"
+player_name = "finn"
+player_health = 60
+player_info = "#{player_name.capitalize}'s health is #{player_health}"
+puts player_info.center(50, '*')
+puts "#{player_name.capitalize.ljust(30, '.')} #{player_health} health"
+
+wacky_string = "  \n finn \t "
+puts wacky_string.strip
 
 puts "🚀" * 25
