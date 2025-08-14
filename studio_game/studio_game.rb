fb14c56e3cea192569f3fc836941cdfad8f8de4b
hgsgtk <kazuki@autify.com>
2025-08-14 16:22:27 +0900

    Refactor game output to use a greeting method and remove countdown; display player health using a structured approach

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,12 +1,6 @@
-puts "Let's play a game\n\n\t3\n\t2\n\t1\n\n"
+def say_hello(name:, health: 100)
+  "I'm #{name.capitalize} with a health of #{health}"
+end
 
-player_name = "finn"
-player_health = 60
-player_info = "#{player_name.capitalize}'s health is #{player_health}"
-puts player_info.center(50, '*')
-puts "#{player_name.capitalize.ljust(30, '.')} #{player_health} health"
-
-wacky_string = "  \n finn \t "
-puts wacky_string.strip
-
-puts "🚀" * 25
+puts say_hello(name: "finn", health: 60)
+puts say_hello(name: "jake")
