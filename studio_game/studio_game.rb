8bb29a64708cc2481ce918463d1ba73e8b12f84b
hgsgtk <kazuki@autify.com>
2025-08-14 15:57:39 +0900

    Enhance game introduction by adding countdown and player health display

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,2 +1,7 @@
-puts "Let's play a game!"
+puts "Let's play a game\n\n\t3\n\t2\n\t1\n\n"
+
+first_player = "Finn"
+first_player_health = 60
+puts "#{first_player}'s health is #{first_player_health / 9}"
+
 puts "🚀" * 25
