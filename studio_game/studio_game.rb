f90ca04baa2d80846e624ee2d9223e621174008b
hgsgtk <kazuki@autify.com>
2025-08-20 13:49:48 +0900

    Refactor player management by adding multiple players to the game; implement player actions within an iteration and update output to reflect player states before and after gameplay.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -27,22 +27,33 @@ class Player
 end
 
 player_1 = Player.new('finn', 60)
+player_2 = Player.new('lucy', 90)
+player_3 = Player.new('jase')
+player_4 = Player.new('alex', 125)
 
-number_rolled = rand(1..6)
-case number_rolled
-when 1..2
-    player_1.drain
-    puts "(#{player_1.name}) got drained 😩"
-when 3..4
-    puts "(#{player_1.name}) got skipped"
-when 5..6
-    player_1.boost
-    puts "(#{player_1.name}) got boosted 💪"
+players = [player_1, player_2, player_3, player_4]
+
+players.pop
+player_5 = Player.new('cole', 75)
+players.push(player_5)
+
+puts "Before playing:"
+puts players
+
+players.each do |player|
+    number_rolled = rand(1..6)
+
+    case number_rolled
+    when 1..2
+        player_1.drain
+        puts "(#{player_1.name}) got drained 😩"
+    when 3..4
+        puts "(#{player_1.name}) got skipped"
+    when 5..6
+        player_1.boost
+        puts "(#{player_1.name}) got boosted 💪"
+    end    
 end
 
-number_rolled = rand(1..7)
-puts number_rolled == 7 ? "lucky" : "unlucky"
-unless number_rolled == 7
-  puts "You rolled a #{number_rolled}"
-end
-puts "Champ!" unless player_1.score <= 250
+puts "\nAfter playing:"
+puts players
