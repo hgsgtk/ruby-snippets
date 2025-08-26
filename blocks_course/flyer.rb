fe95c7590692b45b3fdac2aff29b4cc5f0d9e448
hgsgtk <kazuki@autify.com>
2025-08-26 11:47:21 +0900

    feat: enhance Flyer class with status attribute and update output format; initialize status with a default value and display it in the flyer information.

--- blocks_course/flyer.rb
+++ blocks_course/flyer.rb
@@ -1,23 +1,26 @@
 class Flyer
   attr_reader :name, :email, :miles_flown
+  attr_accessor :status
 
-  def initialize(name, email, miles_flown)
+  def initialize(name, email, miles_flown, status=:bronze)
     @name = name
     @email = email
     @miles_flown = miles_flown
+    @status = status
   end
 
   def to_s
-    "#{name} (#{email}): #{miles_flown}"
+    "#{name} (#{email}): #{miles_flown} - #{status}"
   end
 end
 
 if __FILE__ == $0
   flyers = []
+  statuses = [:bronze, :silver, :gold, :platinum]
   1.upto(5) do |count|
-    flyers.push(Flyer.new("Flyer #{count}", "flyer#{count}@example.com", count * 1000))
+    flyers.push(Flyer.new("Flyer #{count}", "flyer#{count}@example.com", count * 1000, statuses.sample))
   end
-  1.upto(5) { |count| flyers.push(Flyer.new("Flyer #{count}", "flyer#{count}@example.com", count * 1000)) }
+  1.upto(5) { |count| flyers.push(Flyer.new("Flyer #{count}", "flyer#{count}@example.com", count * 1000, statuses.sample)) }
 
   puts flyers
 
