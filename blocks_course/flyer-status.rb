f3fcd18e64f571b347bc8d381fa40002bc368c5e
hgsgtk <kazuki@autify.com>
2025-08-26 15:24:42 +0900

    feat: enhance flyer status output; add partitioning of platinum flyers, name tags with statuses, and total miles calculation in flyer-status.rb

--- blocks_course/flyer-status.rb
+++ blocks_course/flyer-status.rb
@@ -39,3 +39,18 @@ end
 puts 'First bronze flyer:'
 first_bronze_flyer = flyers.detect { |flyer| flyer.status == :bronze }
 puts first_bronze_flyer
+
+platunium_flyers, other_flyers = flyers.partition { |flyer| flyer.status == :platinum }
+puts platunium_flyers, other_flyers
+
+name_tags = flyers.map { |flyer| "#{flyer.name} (#{flyer.status.upcase})" }
+puts name_tags
+
+totals_in_km = flyers.map { |flyer| flyer.miles_flown * 1.6 }
+puts totals_in_km
+
+total_miles = flyers.reduce(0) { |sum, flyer| sum + flyer.miles_flown }
+puts total_miles
+
+total_kms = totals_in_km.reduce(0, :+)
+puts total_kms
