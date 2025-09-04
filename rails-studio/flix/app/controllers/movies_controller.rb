bf9121ce97f7fb6b85eddf83bb096be07db22d5c
hgsgtk <kazuki@autify.com>
2025-09-04 17:14:09 +0900

    feat: implement movie editing functionality

--- rails-studio/flix/app/controllers/movies_controller.rb
+++ rails-studio/flix/app/controllers/movies_controller.rb
@@ -7,4 +7,26 @@ class MoviesController < ApplicationController
     movie_id = params[:id]
     @movie = Movie.find(movie_id)
   end
+
+  def edit
+    movie_id = params[:id]
+    @movie = Movie.find(movie_id)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    # Approach 1: Whitelisted
+    # movie_data = params[:movie].permit(:title, :description, :released_on, :rating, :total_gross)
+
+    # Approach 2: Require
+    # movie_data = params.require(:movie).permit(:title, :description, :released_on, :rating, :total_gross)
+
+    # Approach 3: All allowed
+    movie_data = params.require(:movie).permit!
+
+    @movie.update(movie_data)
+
+    redirect_to @movie
+  end
 end
