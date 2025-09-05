1023127f2ee50fccb572779d2100e1c032f00356
hgsgtk <kazuki@autify.com>
2025-09-05 22:33:38 +0900

    feat: associate reviews with users and update review display in views

--- rails-studio/flix/app/controllers/reviews_controller.rb
+++ rails-studio/flix/app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :require_signin
   before_action :set_movie
 
   def index
@@ -11,6 +12,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @movie.reviews.new(review_params)
+    @review.user = current_user
 
     if @review.save
       redirect_to @movie, notice: "Review successfully created!"
@@ -22,7 +24,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:name, :stars, :comment)
+    params.require(:review).permit(:stars, :comment)
   end
 
   def set_movie
