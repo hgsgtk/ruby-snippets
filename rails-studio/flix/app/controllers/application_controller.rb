836985f7f5bfbd100382dd38b8656a3fedbeec71
hgsgtk <kazuki@autify.com>
2025-09-05 21:21:16 +0900

    feat: add user authentication

--- rails-studio/flix/app/controllers/application_controller.rb
+++ rails-studio/flix/app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   add_flash_types :danger
+
+  private
+
+  def require_signin
+    unless current_user
+      redirect_to signin_path, alert: "Please sign in first!"
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
