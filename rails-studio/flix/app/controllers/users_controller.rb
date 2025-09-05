c8f3d4067e7a31808ebe273cf078d7f0ecba2503
hgsgtk <kazuki@autify.com>
2025-09-05 21:55:09 +0900

    feat: restrict user account deletion to admin users and update user actions in views

--- rails-studio/flix/app/controllers/users_controller.rb
+++ rails-studio/flix/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: [ :new, :create ]
-  before_action :require_correct_user, only: [ :edit, :update, :destroy ]
+  before_action :require_correct_user, only: [ :edit, :update ]
+  before_action :require_admin, only: [ :destroy ]
 
   def index
     @users = User.all
@@ -36,6 +37,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to movies_url, status: :see_other, alert: "Account successfully deleted!"
